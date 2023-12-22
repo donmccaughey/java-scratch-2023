5f3b69c64847ae7c79f1ec34284150a3b5d26c87
Don McCaughey <don@donm.cc>
2023-12-22 13:01:50 -0800

    Added `Word` and `Space` records.
    Also added `Text` interface to enable polymorphism over `Word` and
    `Space`.  Added a `tokenize()` method to `Translator`.

--- igpay/src/cc/donm/igpay/Translator.java
+++ igpay/src/cc/donm/igpay/Translator.java
@@ -1,13 +1,45 @@
 package cc.donm.igpay;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Translator {
+    public static final Pattern WORD_SPLIT = Pattern.compile("\\w+|\\W+");
+
+    public static List<Text> tokenize(String line) {
+        var text = new ArrayList<Text>();
+        var matcher = WORD_SPLIT.matcher(line);
+        while (matcher.find()) {
+            Text token = null;
+            var match = matcher.group();
+            if (match.matches("\\w+")) {
+                token = new Word(match);
+            } else {
+                token = new Space(match);
+            }
+            text.add(token);
+        }
+        return text;
+    }
+
     public void run() {
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                System.out.println(line);
+                var tokens = tokenize(line);
+                for (var token : tokens) {
+                    String description = null;
+                    if (token instanceof Word) {
+                        var text = ((Word) token).word();
+                        description = String.format("word: '%s'", text);
+                    } else {
+                        var space = ((Space) token).space();
+                        description = String.format("space: '%s'", space);
+                    }
+                    System.out.println(description);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
